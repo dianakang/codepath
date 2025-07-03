73a9031883a41090a23caa0ff0c0640f37b83d49
Diana Kang <younjung952@hufs-gsuite.kr>
2025-07-03 19:52:01 -0400

    July 3, 2025

---
+++ Unit 5 (Linked Lists)/Session2.py
@@ -0,0 +1,84 @@
+# # Problem 1: Mutual Friends
+# class Villager:
+#     def __init__(self, name, species, catchphrase):
+#         self.name = name
+#         self.species = species
+#         self.catchphrase = catchphrase
+#         self.friends = []
+
+#     def get_mutuals(self, new_contact):
+#         mutuals = []
+#         for friend in self.friends:
+#             if friend in new_contact.friends:
+#                 mutuals.append(friend.name)
+#         return mutuals
+
+# bob = Villager("Bob", "Cat", "pthhhpth")
+# marshal = Villager("Marshal", "Squirrel", "sulky")
+# ankha = Villager("Ankha", "Cat", "me meow")
+# fauna = Villager("Fauna", "Deer", "dearie")
+# raymond = Villager("Raymond", "Cat", "crisp")
+# stitches = Villager("Stitches", "Cub", "stuffin")
+
+# bob.friends = [stitches, raymond, fauna]
+# marshal.friends = [raymond, ankha, fauna]
+# print(bob.get_mutuals(marshal))
+
+# ankha.friends = [marshal]
+# print(bob.get_mutuals(ankha))
+
+
+# # Problem 2: Linked Up
+# class Node:
+#     def __init__(self, value, next=None):
+#         self.value = value
+#         self.next = next
+
+# # For testing
+# def print_linked_list(head):
+#     current = head
+#     while current:
+#         print(current.value, end=" -> " if current.next else "\n")
+#         current = current.next
+
+# kk_slider = Node("K.K. Slider")
+# harriet = Node("Harriet")
+# saharah = Node("Saharah")
+# isabelle = Node("Isabelle")
+
+# # Add code here to link the above nodes
+# kk_slider.next = harriet
+# harriet.next = saharah
+# saharah.next = isabelle
+
+# print_linked_list(kk_slider)
+
+
+# # Problem 3: Daily Tasks
+# class Node:
+#     def __init__(self, value, next=None):
+#         self.value = value
+#         self.next = next
+
+# # For testing
+# def print_linked_list(head):
+#     current = head
+#     while current:
+#         print(current.value, end=" -> " if current.next else "\n")
+#         current = current.next
+
+# def add_first(head, task):
+#     new_node = Node(task)
+#     new_node.next = head
+#     return new_node
+
+
+# task_1 = Node("shake tree")
+# task_2 = Node("dig fossils")
+# task_3 = Node("catch bugs")
+# task_1.next = task_2
+# task_2.next = task_3
+
+# # Linked List: shake tree -> dig fossils -> catch bugs
+# print_linked_list(add_first(task_1, "check turnip prices"))
+
